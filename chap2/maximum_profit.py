680f3da7cfd6241f06cadcab0bb86b52d2c790c7
YutaOikawa <oikenfight@gmail.com>
2017-07-17 10:41:36 +0900

    first commit

---
+++ chap2/maximum_profit.py
@@ -0,0 +1,1 @@
+N = int(input())R = [int(input()) for i in range(N)]maxv = -20000000000minv = R[0]for i in range(1, N):    maxv = max(R[i] - minv, maxv)    minv = min(R[i], minv)print(maxv)
